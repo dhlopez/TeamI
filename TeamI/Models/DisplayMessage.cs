8769ecff4803ce9503f67d57ec1e1fbe28cf952b
David Lopez <dhelaman@hotmail.com>
2017-01-23 14:53:23 -0500

    Outlook changes

---
+++ TeamI/Models/DisplayMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamI.Models
+{
+    public class DisplayMessage
+    {
+        
+        public string Subject { get; set; }
+        public DateTimeOffset ReceivedDateTime { get; set; }
+
+        public DisplayMessage(string subject, DateTimeOffset? dateTimeReceived)
+        {
+            this.Subject = subject;
+            this.ReceivedDateTime = (DateTimeOffset)dateTimeReceived;
+        }
+    }
+}
