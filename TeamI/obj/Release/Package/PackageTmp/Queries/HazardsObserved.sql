a80c147e42e5563e7b3b07f5705a56147ceca3e3
David Lopez <dhelaman@hotmail.com>
2017-02-06 01:21:39 -0500

    Additional change for hazards, and republish to azure

---
+++ TeamI/obj/Release/Package/PackageTmp/Queries/HazardsObserved.sql
@@ -0,0 +1,5 @@
+﻿
+insert into [HAZARDOBSERVED](HazardDescription, FormID) values('Possible trips or slips', 1)
+insert into [HAZARDOBSERVED](HazardDescription, FormID) values('Area / Equipment neat and tidy', 1)
+insert into [HAZARDOBSERVED](HazardDescription, FormID) values('SOP in place', 1)
+insert into [HAZARDOBSERVED](HazardDescription, FormID) values('Other, specify', 1)
---
+++ TeamI/obj/Release/Package/PackageTmp/Scripts/navigation.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
+
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
+
+
+//below code for tabs
+function openInspection(evt, optName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(optName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    
+}
+
+if(document.getElementById("defaultOpen")!=null)
+{
+    document.getElementById("defaultOpen").click();
+}
