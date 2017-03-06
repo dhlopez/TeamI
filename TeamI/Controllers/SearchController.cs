d506729e5e90af12534f13c2a41fd6a864cfa595
dtope1 <dtope1@M20113H0546>
2017-03-06 13:20:15 -0500

    Added search page with searchable tables on it, still need to add search functionallity, need to talk to you guys to see what we want to do for it

---
+++ TeamI/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TeamI.Models;
+using TeamI.ViewModel;
+
+namespace TeamI.Controllers
+{
+    public class SearchController : Controller
+    {
+        private NCSafteyInspectionEntities db = new NCSafteyInspectionEntities();
+
+        // GET: Search
+        public ActionResult Index()
+        {
+            
+            var inspection = db.INSPECTION.Include(i => i.LAB).Include(i => i.USER);
+            var inspectionDetails = db.INSPECTIONDETAILS;
+            var hazardsObserved = db.HAZARDOBSERVED;
+            return View(new SearchVM(inspection.ToList(), inspectionDetails.ToList(), hazardsObserved.ToList()));
+        }
+
+        // GET: Search/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            INSPECTION iNSPECTION = db.INSPECTION.Find(id);
+            if (iNSPECTION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iNSPECTION);
+        }
+    }
+}
---
+++ TeamI/ViewModel/SearchVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeamI.Models;
+
+namespace TeamI.ViewModel
+{
+    public class SearchVM
+    {
+        private NCSafteyInspectionEntities db = new NCSafteyInspectionEntities();
+        public List<INSPECTION> inspection;
+        public List<INSPECTIONDETAILS> inspectionDetails;
+        public List<HAZARDOBSERVED> hazardsObserved;
+
+        public SearchVM(List<INSPECTION> inspection, List<INSPECTIONDETAILS> inspectionDetails, List<HAZARDOBSERVED> hazardsObserved)
+        {
+            this.inspection = inspection;
+            this.inspectionDetails = inspectionDetails;
+            this.hazardsObserved = hazardsObserved;
+        }
+
+    }
+}
