f9a496e6ec3af0a4cfe58be7b3fc272ec1d072dd
John VanGeemen <johnvdev@hotmail.com>
2017-03-27 13:57:44 -0400

    Budle config updates, added some scripts/css for page tour, not implemented yet.

---
+++ TeamI/Scripts/circleStats/CircleStats.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    if ($.cookie('UserFontSize')) {
+        $('body').css('font-size', $.cookie('UserFontSize'));
+    }
+
+    $("#circle1").circliful({
+        animationStep: 5,
+        foregroundBorderWidth: 5,
+        backgroundBorderWidth: 15,
+        text: "Passed",
+        backgroundcolor: "#1a1a1a",
+        foregroundColor: "Green"
+
+
+    });
+
+    $("#circle2").circliful({
+        animationStep: 5,
+        foregroundBorderWidth: 5,
+        backgroundBorderWidth: 15,
+        text: "Failed",
+        foregroundColor: "Red"
+    });
+
+    $("#circle3").circliful({
+        animationStep: 5,
+        foregroundBorderWidth: 5,
+        backgroundBorderWidth: 15,
+
+        text: "Total",
+        foregroundColor: "#0073cf"
+    });
+});
