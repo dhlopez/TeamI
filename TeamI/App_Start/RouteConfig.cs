a946a6385b2f3e106217e87fffbe168a4ec74c6e
dtope1 <dtope1@M20113H0545>
2017-04-10 13:56:47 -0400

    pdf stuff

--- TeamI/App_Start/RouteConfig.cs
+++ TeamI/App_Start/RouteConfig.cs
@@ -12,13 +12,13 @@ namespace TeamI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapPageRoute("LocalLogin", "LocalLogin", "~/LocalLogin/Welcome.aspx");
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            
+            routes.MapPageRoute("LocalLogin", "LocalLogin", "~/LocalLogin/Welcome.aspx");
         }
     }
 }
