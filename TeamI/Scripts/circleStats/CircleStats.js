bdb8d0c2f9b999b057217ff044803649bcef2005
John VanGeemen <johnvdev@hotmail.com>
2017-04-16 19:24:25 -0400

    Revert "updates"
    This reverts commit 82f5309831901b58d8118a484b96eba4665b0940.

--- TeamI/Scripts/circleStats/CircleStats.js
+++ TeamI/Scripts/circleStats/CircleStats.js
@@ -1,26 +1,33 @@
 ﻿$(document).ready(function () {
 
-
     $("#circle1").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-        text: "Pending",
-        foregroundColor: "purple"
+        text: "Complete",
+        foregroundColor: "lime"
     });
     $("#circle2").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-        text: "Overdue",
-        foregroundColor: "orange"
+
+        text: "Pending",
+        foregroundColor: "purple"
     });
     $("#circle3").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-        text: "Pass Rate",
-        foregroundColor: "Red",
-        decimals: 2
+
+        text: "Overdue",
+        foregroundColor: "orange"
+    });
+    $("#circle4").circliful({
+        animationStep: 5,
+        foregroundBorderWidth: 5,
+        backgroundBorderWidth: 15,
+        text: "Failed",
+        foregroundColor: "Red"
     });
 });
