f4f8b78e9ef30e594838baebbf68fb4da45cd35c
John VanGeemen <johnvdev@hotmail.com>
2017-04-16 20:57:59 -0400

    Circle stats Final, using controler instead of garbage code in cshtml.

--- TeamI/Scripts/circleStats/CircleStats.js
+++ TeamI/Scripts/circleStats/CircleStats.js
@@ -1,33 +1,28 @@
 ﻿$(document).ready(function () {
 
+
     $("#circle1").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-        text: "Complete",
-        foregroundColor: "lime"
+        noPercentageSign: 1,
+        text: "Pending",
+        foregroundColor: "Blue"
     });
     $("#circle2").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-
-        text: "Pending",
-        foregroundColor: "purple"
+        noPercentageSign:1,
+        text: "Overdue",
+        foregroundColor: "orange"
     });
     $("#circle3").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
         backgroundBorderWidth: 15,
-
-        text: "Overdue",
-        foregroundColor: "orange"
-    });
-    $("#circle4").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 5,
-        backgroundBorderWidth: 15,
-        text: "Failed",
-        foregroundColor: "Red"
+        text: "Pass Rate",
+        foregroundColor: "Green",
+        decimals:2
     });
 });
